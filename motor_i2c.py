4e903146267635519b295a2a874ff547752d1f27
Devin Kelly <dwwkelly@gmail.com>
2013-04-06 15:52:08 -0400

    cleaned up some of the motor_i2c stuff, initializing the bus by function now

--- motor_i2c.py
+++ motor_i2c.py
@@ -1,28 +1,31 @@
 import smbus
 
-bus = smbus.SMBus(0)
-address = 15
 
-def SetMotorSpeed(speeda, speedb, address=address, bus=bus):
-	# speeds can be set from -255 to 255
-	MotorSpeedSet =		0x82
-	PWMFrequenceSet =	0x84
-	DirectionSet =		0xaa
-	MotorSetA =			0xa1
-	MotorSetB =			0xa5
-	Nothing =			0x01
-	I2CMotorDriverAdd =	0x0f
+def setupSMBus(busNumber=0):
+   bus = smbus.SMBus(busNumber)
+   return bus
 
-	direction = ''
-	if speeda >= 0:
-		direction += '0b01'
-	else:
-		direction += '0b10'
-	if speedb >= 0:
-		direction += '10'
-	else:
-		direction += '01'
-	direction = eval(direction)
 
-	bus.write_i2c_block_data(address,DirectionSet,[direction])
-	bus.write_i2c_block_data(address,MotorSpeedSet,[abs(speeda), abs(speedb)])
+def SetMotorSpeed(speeda, speedb, bus, address=15):
+   # speeds can be set from -255 to 255
+   MotorSpeedSet = 0x82
+   PWMFrequenceSet = 0x84
+   DirectionSet = 0xaa
+   MotorSetA = 0xa1
+   MotorSetB = 0xa5
+   Nothing = 0x01
+   I2CMotorDriverAdd = 0x0f
+
+   direction = ''
+   if speeda >= 0:
+      direction += '0b01'
+   else:
+      direction += '0b10'
+   if speedb >= 0:
+      direction += '10'
+   else:
+      direction += '01'
+   direction = eval(direction)
+
+   bus.write_i2c_block_data(address, DirectionSet, [direction])
+   bus.write_i2c_block_data(address, MotorSpeedSet, [abs(speeda), abs(speedb)])
